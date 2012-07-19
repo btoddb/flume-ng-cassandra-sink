ebd5f23b9eccc1eb0126f9277f321e18ca06ee5d
btoddb <btoddb@gmail.com>
2012-07-19 08:57:46 -0700

    added logging and defaults if headers don't exist

--- src/main/java/org/apache/flume/sink/cassandra/CassandraSinkRepository.java
+++ src/main/java/org/apache/flume/sink/cassandra/CassandraSinkRepository.java
@@ -54,263 +54,264 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
-import org.joda.time.format.ISODateTimeFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CassandraSinkRepository {
-	private static DateTimeFormatter dfEvent = ISODateTimeFormat.dateTime()
-			.withOffsetParsed();
-	private static DateTimeFormatter dfHourKey = new DateTimeFormatterBuilder()
-			.appendYear(4, 4).appendMonthOfYear(2).appendDayOfMonth(2)
-			.appendHourOfDay(2).toFormatter();
+    private static final Logger logger = LoggerFactory.getLogger(CassandraSinkRepository.class);
 
-	// all reads/writes for cached data are ONE, meta reads/writes are at quorum
-	private static final ConsistencyLevelPolicy CONSISTENCY_LEVEL_POLICY = new ConsistencyLevelPolicy() {
-		@Override
-		public HConsistencyLevel get(OperationType op, String cfName) {
-			return get(op);
-		}
+    // private static DateTimeFormatter dfEvent = ISODateTimeFormat.dateTime()
+    // .withOffsetParsed();
+    private static DateTimeFormatter dfHourKey = new DateTimeFormatterBuilder().appendYear(4, 4).appendMonthOfYear(2)
+            .appendDayOfMonth(2).appendHourOfDay(2).toFormatter();
 
-		@Override
-		public HConsistencyLevel get(OperationType op) {
-			if (OperationType.READ == op) {
-				return HConsistencyLevel.ONE;
-			} else if (OperationType.WRITE == op) {
-				return HConsistencyLevel.ONE;
-			} else {
-				return HConsistencyLevel.QUORUM;
-			}
-		}
-	};
+    // all reads/writes for cached data are ONE, meta reads/writes are at quorum
+    private static final ConsistencyLevelPolicy CONSISTENCY_LEVEL_POLICY = new ConsistencyLevelPolicy() {
+        @Override
+        public HConsistencyLevel get(OperationType op, String cfName) {
+            return get(op);
+        }
 
-	private static final Object EMPTY_BYTE_ARRAY = new byte[0];
+        @Override
+        public HConsistencyLevel get(OperationType op) {
+            if (OperationType.READ == op) {
+                return HConsistencyLevel.ONE;
+            }
+            else if (OperationType.WRITE == op) {
+                return HConsistencyLevel.ONE;
+            }
+            else {
+                return HConsistencyLevel.QUORUM;
+            }
+        }
+    };
 
-	private String hosts;
-	private int port;
-	private String clusterName;
-	private String keyspaceName;
-	private int socketTimeoutMillis;
-	private int maxConnectionsPerHost;
-	private int maxExhaustedWaitMillis;
-	private String hoursColFamName = "hours";
-	private String recordsColFamName = "records";
+    private static final Object EMPTY_BYTE_ARRAY = new byte[0];
+    private static final String HEADER_TIMESTAMP = "timestamp";
+    private static final String HEADER_SOURCE = "src";
+    private static final String HEADER_HOST = "host";
 
-	private Cluster cluster;
-	private Keyspace keyspace;
+    private String hosts;
+    private int port;
+    private String clusterName;
+    private String keyspaceName;
+    private int socketTimeoutMillis;
+    private int maxConnectionsPerHost;
+    private int maxExhaustedWaitMillis;
+    private String hoursColFamName = "hours";
+    private String recordsColFamName = "records";
 
-	public void initHector() {
-		// TODO:btb - go thru these later to be sure they are set correct
-		// and properly parameterized by config file
-		CassandraHostConfigurator cassConfig = new CassandraHostConfigurator();
-		cassConfig.setAutoDiscoverHosts(true);
-		cassConfig.setAutoDiscoveryDelayInSeconds(600); // 5 minutes
-		cassConfig.setCassandraThriftSocketTimeout(socketTimeoutMillis);
-		cassConfig.setHosts(hosts);
-		cassConfig.setMaxActive(maxConnectionsPerHost);
-		cassConfig.setMaxWaitTimeWhenExhausted(maxExhaustedWaitMillis);
-		cassConfig.setOpTimer(new SpeedForJOpTimer(clusterName));
-		cassConfig.setPort(port);
-		cassConfig.setRunAutoDiscoveryAtStartup(true);
-		cassConfig.setUseThriftFramedTransport(true);
-		cassConfig.setRetryDownedHosts(true);
-		cassConfig.setRetryDownedHostsDelayInSeconds(10);
-		cassConfig.setRetryDownedHostsQueueSize(-1); // no bounds
+    private Cluster cluster;
+    private Keyspace keyspace;
 
-		cluster = HFactory.createCluster(clusterName, cassConfig, null);
-		keyspace = HFactory.createKeyspace(keyspaceName, cluster,
-				CONSISTENCY_LEVEL_POLICY);
-	}
+    public void initHector() {
+        // TODO:btb - go thru these later to be sure they are set correct
+        // and properly parameterized by config file
+        CassandraHostConfigurator cassConfig = new CassandraHostConfigurator();
+        cassConfig.setAutoDiscoverHosts(true);
+        cassConfig.setAutoDiscoveryDelayInSeconds(600); // 5 minutes
+        cassConfig.setCassandraThriftSocketTimeout(socketTimeoutMillis);
+        cassConfig.setHosts(hosts);
+        cassConfig.setMaxActive(maxConnectionsPerHost);
+        cassConfig.setMaxWaitTimeWhenExhausted(maxExhaustedWaitMillis);
+        cassConfig.setOpTimer(new SpeedForJOpTimer(clusterName));
+        cassConfig.setPort(port);
+        cassConfig.setRunAutoDiscoveryAtStartup(true);
+        cassConfig.setUseThriftFramedTransport(true);
+        cassConfig.setRetryDownedHosts(true);
+        cassConfig.setRetryDownedHostsDelayInSeconds(10);
+        cassConfig.setRetryDownedHostsQueueSize(-1); // no bounds
 
-	public void saveToCassandra(List<Event> eventList) {
-		Mutator<ByteBuffer> m = HFactory.createMutator(keyspace,
-				ByteBufferSerializer.get());
-		for (Event event : eventList) {
-			Map<String, String> headers = event.getHeaders();
+        cluster = HFactory.createCluster(clusterName, cassConfig, null);
+        keyspace = HFactory.createKeyspace(keyspaceName, cluster, CONSISTENCY_LEVEL_POLICY);
+    }
 
-			DateTime ts = getTime(headers.get("timestamp"));
-			String src = null != headers.get("src") ? headers.get("src") : "not-set";
-			String host = headers.get("host");
+    public void saveToCassandra(List<Event> eventList) {
+        Mutator<ByteBuffer> m = HFactory.createMutator(keyspace, ByteBufferSerializer.get());
+        for (Event event : eventList) {
+            Map<String, String> headers = event.getHeaders();
 
-			UUID tsUuid = createTimeUUID(ts);
-			String hour = getHourFromTime(ts);
+            DateTime ts;
+            if (headers.containsKey(HEADER_TIMESTAMP)) {
+                ts = getTime(headers.get("timestamp"));
+            }
+            else {
+                ts = new DateTime();
+                logger.info("Event is missing '" + HEADER_TIMESTAMP + "' header - using current time");
+            }
+            String src = headers.containsKey(HEADER_SOURCE) ? headers.get(HEADER_SOURCE) : "not-set";
+            String host = headers.containsKey(HEADER_HOST) ? headers.get(HEADER_HOST) : "not-set";
 
-			ByteBuffer recordsKey = TimeUUIDUtils.asByteBuffer(tsUuid);
-			m.addInsertion(recordsKey, recordsColFamName,
-					HFactory.createColumn("ts", ts.getMillis()));
-			m.addInsertion(recordsKey, recordsColFamName,
-					HFactory.createColumn("src", src));
-			m.addInsertion(recordsKey, recordsColFamName,
-					HFactory.createColumn("host", host));
-			m.addInsertion(recordsKey, recordsColFamName,
-					HFactory.createColumn("data", event.getBody()));
+            UUID tsUuid = createTimeUUID(ts);
+            String hour = getHourFromTime(ts);
 
-			m.addInsertion(ByteBuffer.wrap(hour.getBytes()), hoursColFamName,
-					HFactory.createColumn(tsUuid, EMPTY_BYTE_ARRAY));
-		}
-		m.execute();
-	}
+            ByteBuffer recordsKey = TimeUUIDUtils.asByteBuffer(tsUuid);
+            m.addInsertion(recordsKey, recordsColFamName, HFactory.createColumn("ts", ts.getMillis()));
+            m.addInsertion(recordsKey, recordsColFamName, HFactory.createColumn("src", src));
+            m.addInsertion(recordsKey, recordsColFamName, HFactory.createColumn("host", host));
+            m.addInsertion(recordsKey, recordsColFamName, HFactory.createColumn("data", event.getBody()));
 
-	private String getHourFromTime(DateTime ts) {
-		return dfHourKey.print(ts.withZone(DateTimeZone.UTC));
-	}
+            m.addInsertion(ByteBuffer.wrap(hour.getBytes()), hoursColFamName,
+                    HFactory.createColumn(tsUuid, EMPTY_BYTE_ARRAY));
+        }
+        m.execute();
+    }
 
-	private DateTime getTime(String tsAsStr) {
-		if (null != tsAsStr && !tsAsStr.isEmpty()) {
-//			return dfEvent.parseDateTime(tsAsStr);
-			return new DateTime(Long.parseLong(tsAsStr));
-		} else {
-			return new DateTime();
-		}
+    private String getHourFromTime(DateTime ts) {
+        return dfHourKey.print(ts.withZone(DateTimeZone.UTC));
+    }
 
-	}
+    private DateTime getTime(String tsAsStr) {
+        if (null != tsAsStr && !tsAsStr.isEmpty()) {
+            // return dfEvent.parseDateTime(tsAsStr);
+            return new DateTime(Long.parseLong(tsAsStr));
+        }
+        else {
+            return new DateTime();
+        }
 
-	private UUID createTimeUUID(DateTime ts) {
-		return TimeUUIDUtils.getTimeUUID(ts.getMillis());
-	}
+    }
 
-	public List<LogEvent> getEventsForHour(String hour) {
-		SliceQuery<String, UUID, byte[]> dayQuery = HFactory.createSliceQuery(
-				keyspace, StringSerializer.get(), UUIDSerializer.get(),
-				BytesArraySerializer.get());
-		dayQuery.setColumnFamily(hoursColFamName);
-		dayQuery.setKey(hour);
-		dayQuery.setRange(null, null, false, 100);
+    private UUID createTimeUUID(DateTime ts) {
+        return TimeUUIDUtils.getTimeUUID(ts.getMillis());
+    }
 
-		// TODO:BTB - this will need to be paginated
-		QueryResult<ColumnSlice<UUID, byte[]>> dayResult = dayQuery.execute();
+    public List<LogEvent> getEventsForHour(String hour) {
+        SliceQuery<String, UUID, byte[]> dayQuery = HFactory.createSliceQuery(keyspace, StringSerializer.get(),
+                UUIDSerializer.get(), BytesArraySerializer.get());
+        dayQuery.setColumnFamily(hoursColFamName);
+        dayQuery.setKey(hour);
+        dayQuery.setRange(null, null, false, 100);
 
-		ColumnSlice<UUID, byte[]> daySlice = null != dayResult ? dayResult
-				.get() : null;
-		List<HColumn<UUID, byte[]>> dayColList = null != daySlice ? daySlice
-				.getColumns() : null;
-		if (null != dayColList && !dayColList.isEmpty()) {
-			List<UUID> recordKeyList = new ArrayList<UUID>();
-			for (HColumn<UUID, byte[]> col : dayColList) {
-				recordKeyList.add(col.getName());
-			}
+        // TODO:BTB - this will need to be paginated
+        QueryResult<ColumnSlice<UUID, byte[]>> dayResult = dayQuery.execute();
 
-			// TODO:BTB - this will need to be done in pages
-			return getRecords(recordKeyList);
-		} else {
-			return Collections.emptyList();
-		}
-	}
+        ColumnSlice<UUID, byte[]> daySlice = null != dayResult ? dayResult.get() : null;
+        List<HColumn<UUID, byte[]>> dayColList = null != daySlice ? daySlice.getColumns() : null;
+        if (null != dayColList && !dayColList.isEmpty()) {
+            List<UUID> recordKeyList = new ArrayList<UUID>();
+            for (HColumn<UUID, byte[]> col : dayColList) {
+                recordKeyList.add(col.getName());
+            }
 
-	private List<LogEvent> getRecords(List<UUID> recordKeyList) {
-		MultigetSliceQuery<UUID, String, byte[]> q = HFactory
-				.createMultigetSliceQuery(keyspace, UUIDSerializer.get(),
-						StringSerializer.get(), BytesArraySerializer.get());
-		q.setColumnFamily(recordsColFamName);
-		q.setKeys(recordKeyList);
-		q.setRange(null, null, false, 100);
+            // TODO:BTB - this will need to be done in pages
+            return getRecords(recordKeyList);
+        }
+        else {
+            return Collections.emptyList();
+        }
+    }
 
-		// get the records
-		// TODO:BTB - this will need to be paginated
-		QueryResult<Rows<UUID, String, byte[]>> result = q.execute();
+    private List<LogEvent> getRecords(List<UUID> recordKeyList) {
+        MultigetSliceQuery<UUID, String, byte[]> q = HFactory.createMultigetSliceQuery(keyspace, UUIDSerializer.get(),
+                StringSerializer.get(), BytesArraySerializer.get());
+        q.setColumnFamily(recordsColFamName);
+        q.setKeys(recordKeyList);
+        q.setRange(null, null, false, 100);
 
-		// make sure we got something and dig for the data
-		Rows<UUID, String, byte[]> rows = null != result ? result.get() : null;
-		Iterator<Row<UUID, String, byte[]>> iter = null != rows ? rows
-				.iterator() : null;
-		if (null == iter) {
-			return Collections.emptyList();
-		}
+        // get the records
+        // TODO:BTB - this will need to be paginated
+        QueryResult<Rows<UUID, String, byte[]>> result = q.execute();
 
-		// iterate over the rows returned
-		List<LogEvent> logEventList = new LinkedList<LogEvent>();
-		while (iter.hasNext()) {
-			Row<UUID, String, byte[]> row = iter.next();
-			ColumnSlice<String, byte[]> slice = null != row ? row
-					.getColumnSlice() : null;
-			List<HColumn<String, byte[]>> colList = null != slice ? slice
-					.getColumns() : null;
+        // make sure we got something and dig for the data
+        Rows<UUID, String, byte[]> rows = null != result ? result.get() : null;
+        Iterator<Row<UUID, String, byte[]>> iter = null != rows ? rows.iterator() : null;
+        if (null == iter) {
+            return Collections.emptyList();
+        }
 
-			// it is possible to get a row with no columns (has tombstones)
-			if (null != colList && !colList.isEmpty()) {
-				HColumn<String, byte[]> tmp = slice.getColumnByName("src");
-				String src = new String(tmp.getValue());
-				String host = new String(slice.getColumnByName("host")
-						.getValue());
-				byte[] data = slice.getColumnByName("data").getValue();
+        // iterate over the rows returned
+        List<LogEvent> logEventList = new LinkedList<LogEvent>();
+        while (iter.hasNext()) {
+            Row<UUID, String, byte[]> row = iter.next();
+            ColumnSlice<String, byte[]> slice = null != row ? row.getColumnSlice() : null;
+            List<HColumn<String, byte[]>> colList = null != slice ? slice.getColumns() : null;
 
-				LogEvent logEvt = new LogEvent(
-						TimeUUIDUtils.getTimeFromUUID(row.getKey()), data);
-				logEventList.add(logEvt);
-			}
-		}
+            // it is possible to get a row with no columns (has tombstones)
+            if (null != colList && !colList.isEmpty()) {
+                HColumn<String, byte[]> tmp = slice.getColumnByName("src");
+                String src = new String(tmp.getValue());
+                String host = new String(slice.getColumnByName("host").getValue());
+                byte[] data = slice.getColumnByName("data").getValue();
 
-		return logEventList;
-	}
+                LogEvent logEvt = new LogEvent(TimeUUIDUtils.getTimeFromUUID(row.getKey()), data);
+                logEventList.add(logEvt);
+            }
+        }
 
-	public String getHosts() {
-		return hosts;
-	}
+        return logEventList;
+    }
 
-	public void setHosts(String hosts) {
-		this.hosts = hosts;
-	}
+    public String getHosts() {
+        return hosts;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public void setHosts(String hosts) {
+        this.hosts = hosts;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public String getClusterName() {
-		return clusterName;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public void setClusterName(String clusterName) {
-		this.clusterName = clusterName;
-	}
+    public String getClusterName() {
+        return clusterName;
+    }
 
-	public String getKeyspaceName() {
-		return keyspaceName;
-	}
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
 
-	public void setKeyspaceName(String keyspaceName) {
-		this.keyspaceName = keyspaceName;
-	}
+    public String getKeyspaceName() {
+        return keyspaceName;
+    }
 
-	public int getSocketTimeoutMillis() {
-		return socketTimeoutMillis;
-	}
+    public void setKeyspaceName(String keyspaceName) {
+        this.keyspaceName = keyspaceName;
+    }
 
-	public void setSocketTimeoutMillis(int socketTimeoutMillis) {
-		this.socketTimeoutMillis = socketTimeoutMillis;
-	}
+    public int getSocketTimeoutMillis() {
+        return socketTimeoutMillis;
+    }
 
-	public int getMaxConnectionsPerHost() {
-		return maxConnectionsPerHost;
-	}
+    public void setSocketTimeoutMillis(int socketTimeoutMillis) {
+        this.socketTimeoutMillis = socketTimeoutMillis;
+    }
 
-	public void setMaxConnectionsPerHost(int maxConnectionsPerHost) {
-		this.maxConnectionsPerHost = maxConnectionsPerHost;
-	}
+    public int getMaxConnectionsPerHost() {
+        return maxConnectionsPerHost;
+    }
 
-	public int getMaxExhaustedWaitMillis() {
-		return maxExhaustedWaitMillis;
-	}
+    public void setMaxConnectionsPerHost(int maxConnectionsPerHost) {
+        this.maxConnectionsPerHost = maxConnectionsPerHost;
+    }
 
-	public void setMaxExhaustedWaitMillis(int maxExhaustedWaitMillis) {
-		this.maxExhaustedWaitMillis = maxExhaustedWaitMillis;
-	}
+    public int getMaxExhaustedWaitMillis() {
+        return maxExhaustedWaitMillis;
+    }
 
-	public String getHoursColFamName() {
-		return hoursColFamName;
-	}
+    public void setMaxExhaustedWaitMillis(int maxExhaustedWaitMillis) {
+        this.maxExhaustedWaitMillis = maxExhaustedWaitMillis;
+    }
 
-	public void setHoursColFamName(String hoursColFamName) {
-		this.hoursColFamName = hoursColFamName;
-	}
+    public String getHoursColFamName() {
+        return hoursColFamName;
+    }
 
-	public String getRecordsColFamName() {
-		return recordsColFamName;
-	}
+    public void setHoursColFamName(String hoursColFamName) {
+        this.hoursColFamName = hoursColFamName;
+    }
 
-	public void setRecordsColFamName(String recordsColFameName) {
-		this.recordsColFamName = recordsColFameName;
-	}
+    public String getRecordsColFamName() {
+        return recordsColFamName;
+    }
+
+    public void setRecordsColFamName(String recordsColFameName) {
+        this.recordsColFamName = recordsColFameName;
+    }
 
 }
