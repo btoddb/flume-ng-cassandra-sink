a7c5e607acab34a5bba1b56a4b4150c2f602cdc0
B. Todd Burruss <btoddb@gmail.com>
2012-07-26 13:44:42 -0700

    made multithreaded to improve test app throughput

--- src/main/java/com/btoddb/flume/avroclient/AvroTestClient.java
+++ src/main/java/com/btoddb/flume/avroclient/AvroTestClient.java
@@ -3,9 +3,15 @@ package com.btoddb.flume.avroclient;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
@@ -23,11 +29,12 @@ import com.btoddb.flume.sinks.cassandra.JmxStatsHelper;
 
 public class AvroTestClient implements AvroClientTestMXBean {
     private static final Logger logger = LoggerFactory.getLogger(AvroTestClient.class);
-    
+
     private static final String MBEAN_NAME_ROOT = "com.btoddb.flume.avroclient:type=test-client";
 
     private static final String STAT_REQUESTS = "reqs";
 
+    private int numThreads;
     private String host;
     private int port;
     private int batchSize;
@@ -36,13 +43,20 @@ public class AvroTestClient implements AvroClientTestMXBean {
 
     private MBeanServer mbs;
     private JmxStatsHelper stats;
+    private AtomicLong numProcessed = new AtomicLong();
+
+    private ExecutorService execSrvc;
+    private ArrayBlockingQueue<String> workQueue;
+    private boolean stopProcessing = false;
+    private long start = System.currentTimeMillis();
+    private String clientIp;
 
     public AvroTestClient(String[] args) {
         init(args);
     }
 
     private void init(String[] args) {
-        if (5 != args.length) {
+        if (6 != args.length) {
             showUsage();
             System.exit(-1);
         }
@@ -52,6 +66,7 @@ public class AvroTestClient implements AvroClientTestMXBean {
         batchSize = Integer.parseInt(args[2]);
         iterations = Integer.parseInt(args[3]);
         delay = Integer.parseInt(args[4]);
+        numThreads = Integer.parseInt(args[5]);
 
         if (null == stats) {
             stats = new JmxStatsHelper(5 * 1000);
@@ -64,11 +79,20 @@ public class AvroTestClient implements AvroClientTestMXBean {
         catch (Throwable e) {
             logger.error("exception while registering me as mbean, " + MBEAN_NAME_ROOT, e);
         }
+
+        execSrvc = new ThreadPoolExecutor(numThreads, numThreads, 1, TimeUnit.HOURS,
+                new ArrayBlockingQueue<Runnable>(1));
+
+        workQueue = new ArrayBlockingQueue<String>(numThreads * 2);
+        for (int i = 0; i < numThreads; i++) {
+            execSrvc.submit(new AvroTestTask(i + 1));
+        }
     }
 
     private void showUsage() {
         System.out.println();
-        System.out.println("usage: AvroTestclient <host> <port> <batch-size> <iterations> <delay-in-millis>");
+        System.out
+                .println("usage: AvroTestclient <host> <port> <batch-size> <iterations> <delay-in-millis> <num-threads>");
         System.out.println();
     }
 
@@ -86,48 +110,51 @@ public class AvroTestClient implements AvroClientTestMXBean {
         reader.close();
         String data = sb.toString();
 
+        clientIp = InetAddress.getLocalHost().getHostAddress();
+        
+        String dataFormatter = new String("%s : %09d : %s");
+        
         System.out.println();
-        System.out.println("sample data (" + data.length() + ") : " + data);
+        System.out.println("sample data " + String.format( dataFormatter, clientIp, Integer.valueOf(234), data) );
         System.out.println();
-
-        //
-        // start avro
-        //
-
-        RpcClient rpcClient = RpcClientFactory.getDefaultInstance(host, port, batchSize);
 
         //
         // setup client headers
         //
 
-        Map<String, String> headerMap = new HashMap<String, String>();
-        headerMap.put(FlumeLogEvent.HEADER_SOURCE, "src1");
-
-        try {
-            for (int i = 0; i < iterations; i++) {
-                if (0 == i % 100) {
-                    System.out.println("iteration : " + new Date().toString() + " : " + i + " : " + stats.getStat(STAT_REQUESTS).getCountPerSecond()
-                            + " reqs/sec");
-                }
-                Event event = EventBuilder.withBody(("test event : " + i + " : " + data).getBytes(), headerMap);
-                long start = System.nanoTime();
-//                check-batch-size
-                rpcClient.append(event);
-                stats.update(STAT_REQUESTS, 1, (System.nanoTime() - start) / 1000);
-                try {
-                    Thread.sleep(delay);
-                }
-                catch (InterruptedException e) {
-                    // ignore
-                    Thread.interrupted();
-                }
+        for (int i = 0; i < iterations; i++) {
+            showStats();
+            try {
+                workQueue.put(String.format( dataFormatter, clientIp, Integer.valueOf(i), data));
+            }
+            catch (InterruptedException e) {
+                Thread.interrupted();
             }
         }
-        catch (EventDeliveryException e) {
-            e.printStackTrace();
+
+        while (numProcessed.get() < iterations) {
+            showStats();
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                Thread.interrupted();
+                // ignore
+            }
+        }
+        stopProcessing = true;
+        execSrvc.shutdownNow();
+
+        System.out.println("finished " + iterations + " iterations");
+    }
+
+    private void showStats() {
+        if (2000 < System.currentTimeMillis() - start) {
+            System.out.println("iteration : " + new Date().toString() + " : " + numProcessed.get() + " : "
+                    + stats.getStat(STAT_REQUESTS).getCountPerSecond() + " reqs/sec");
+            start = System.currentTimeMillis();
         }
 
-        rpcClient.close();
     }
 
     public static void main(String[] args) throws Exception {
@@ -183,5 +210,72 @@ public class AvroTestClient implements AvroClientTestMXBean {
     @Override
     public void setIterations(int iterations) {
         this.iterations = iterations;
+    }
+
+    /**
+     * 
+     * @author bburruss
+     * 
+     */
+    class AvroTestTask implements Runnable {
+        private int workerId;
+
+        public AvroTestTask(int workerId) {
+            this.workerId = workerId;
+        }
+
+        @Override
+        public void run() {
+            Thread.currentThread().setName("Test Worker " + workerId);
+            RpcClient rpcClient = createRpcClient();
+            Map<String, String> headerMap = new HashMap<String, String>();
+            headerMap.put(FlumeLogEvent.HEADER_SOURCE, "source" + workerId);
+
+            try {
+                while (!stopProcessing) {
+                    if (Thread.interrupted()) {
+                        continue;
+                    }
+
+                    String data;
+                    try {
+                        data = workQueue.take();
+                    }
+                    catch (InterruptedException e) {
+                        Thread.interrupted();
+                        continue;
+                    }
+
+                    numProcessed.incrementAndGet();
+
+                    try {
+                        Event event = EventBuilder.withBody(data.getBytes(), headerMap);
+                        long start = System.nanoTime();
+                        // check-batch-size
+                        rpcClient.append(event);
+                        stats.update(STAT_REQUESTS, 1, (System.nanoTime() - start) / 1000);
+                        try {
+                            Thread.sleep(delay);
+                        }
+                        catch (InterruptedException e) {
+                            // ignore
+                            Thread.interrupted();
+                        }
+                    }
+                    catch (EventDeliveryException e) {
+                        e.printStackTrace();
+                        rpcClient.close();
+                        rpcClient = RpcClientFactory.getDefaultInstance(host, port, batchSize);
+                    }
+                }
+            }
+            finally {
+                rpcClient.close();
+            }
+        }
+    }
+
+    public RpcClient createRpcClient() {
+        return RpcClientFactory.getDefaultInstance(host, port, batchSize);
     }
 }
--- src/main/java/com/btoddb/flume/sinks/cassandra/JmxStatsHelper.java
+++ src/main/java/com/btoddb/flume/sinks/cassandra/JmxStatsHelper.java
@@ -2,6 +2,7 @@ package com.btoddb.flume.sinks.cassandra;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class JmxStatsHelper {
 
@@ -11,25 +12,53 @@ public class JmxStatsHelper {
     private long windowStartTime;
     private long windowDuration = 15 * 1000;
 
+    private volatile boolean checkingWindow = false;
+    private Object checkingWindowMonitor = new Object();
+    private Object updateMonitor = new Object();
+    private Object createStatMonitor = new Object();
+
     public JmxStatsHelper(long windowDuration) {
         this.windowDuration = windowDuration;
         resetStatsWindow();
     }
 
     public void checkStatsWindow() {
-        if (System.currentTimeMillis() - windowStartTime > windowDuration) {
-            resetStatsWindow();
+        if (checkingWindow) {
+            return;
+        }
+
+        boolean gotAccess = false;
+        synchronized (checkingWindowMonitor) {
+            if (!checkingWindow) {
+                checkingWindow = true;
+                gotAccess = true;
+            }
+        }
+
+        if (gotAccess) {
+            try {
+                if (System.currentTimeMillis() - windowStartTime > windowDuration) {
+                    resetStatsWindow();
+                }
+            }
+            finally {
+                checkingWindow = false;
+            }
         }
     }
 
     public void resetStatsWindow() {
-        reportingMap = calcMap;
+        synchronized (updateMonitor) {
+            reportingMap = calcMap;
+            calcMap = new HashMap<String, JmxStatsHelper.Stat>();
+            windowStartTime = System.currentTimeMillis();
+        }
+
+        // lock down window end time on reporting map
         long endTime = System.currentTimeMillis();
         for (Stat st : reportingMap.values()) {
             st.lock(endTime);
         }
-        calcMap = new HashMap<String, JmxStatsHelper.Stat>();
-        windowStartTime = System.currentTimeMillis();
     }
 
     public void update(String name, int count, long amount) {
@@ -41,8 +70,7 @@ public class JmxStatsHelper {
     private Stat getCalcStat(String name) {
         Stat stat = calcMap.get(name);
         if (null == stat) {
-            stat = new Stat(name, windowStartTime);
-            calcMap.put(name, stat);
+            stat = createNewStat(calcMap, name, windowStartTime);
         }
         return stat;
     }
@@ -50,10 +78,25 @@ public class JmxStatsHelper {
     public Stat getStat(String name) {
         Stat stat = reportingMap.get(name);
         if (null == stat) {
-            stat = new Stat(name, windowStartTime);
-            reportingMap.put(name, stat);
+            stat = createNewStat(reportingMap, name, windowStartTime);
         }
         return stat;
+    }
+
+    private Stat createNewStat(Map<String, Stat> map, String name, long windowStartTime) {
+        Stat stat = map.get(name);
+        if ( null != stat ) {
+            return stat;
+        }
+        
+        synchronized (createStatMonitor) {
+            stat = map.get(name);
+            if (null == stat) {
+                stat = new Stat(name, windowStartTime);
+                map.put(name, stat);
+            }
+            return stat;
+        }
     }
 
     /**
@@ -66,8 +109,10 @@ public class JmxStatsHelper {
         private int count = 0;
         private long amount = 0;
 
-        private long startTime;
-        private long endTime;
+        private volatile long startTime;
+        private volatile long endTime;
+
+        private ReentrantReadWriteLock updateLock = new ReentrantReadWriteLock();
 
         public Stat(String name, long startTime) {
             this.name = name;
@@ -78,9 +123,15 @@ public class JmxStatsHelper {
             return name;
         }
 
-        public void increment(int count, long amount) {
-            this.count += count;
-            this.amount += amount;
+        public void increment(int deltaCount, long deltaAmount) {
+            updateLock.writeLock().lock();
+            try {
+                this.count += deltaCount;
+                this.amount += deltaAmount;
+            }
+            finally {
+                updateLock.writeLock().unlock();
+            }
         }
 
         public void lock(long endTime) {
@@ -96,16 +147,28 @@ public class JmxStatsHelper {
         }
 
         public int getAverageAmount() {
-            return 0 < count ? (int) (amount / count) : 0;
+            updateLock.readLock().lock();
+            try {
+                return 0 < count ? (int) (amount / count) : 0;
+            }
+            finally {
+                updateLock.readLock().unlock();
+            }
         }
 
         public int getCountPerSecond() {
-            long tmp = getEndTime() - startTime;
-            if (0 < tmp) {
-                return (int) (count / (tmp / 1000.0));
+            updateLock.readLock().lock();
+            try {
+                long tmp = getEndTime() - startTime;
+                if (0 < tmp) {
+                    return (int) (count / (tmp / 1000.0));
+                }
+                else {
+                    return 0;
+                }
             }
-            else {
-                return 0;
+            finally {
+                updateLock.readLock().unlock();
             }
         }
 
