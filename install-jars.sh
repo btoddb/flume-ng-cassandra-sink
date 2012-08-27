11e7b18058d8071f2f20d7b6e84a2279b8f7733e
B. Todd Burruss <btoddb@gmail.com>
2012-08-27 16:29:57 -0700

    changed to flume-ng-cassandra-sink

--- install-jars.sh
+++ install-jars.sh
@@ -1,48 +1,26 @@
 FLUME_SERVER_LIB_DIR=/btoddb/flume-dist/flume-server-node/lib
 FLUME_CLIENT_LIB_DIR=/btoddb/flume-dist/flume-client-node/lib
 
-rm ${FLUME_SERVER_LIB_DIR}/*
-rm ${FLUME_CLIENT_LIB_DIR}/*
+#rm ${FLUME_SERVER_LIB_DIR}/*
+#rm ${FLUME_CLIENT_LIB_DIR}/*
 
 rm -r tmp-staging > /dev/null
 mkdir tmp-staging
 pushd tmp-staging > /dev/null
-tar xvfz ../target/flume-cassandra-sink-1.0.0-SNAPSHOT-dist.tar.gz > /dev/null
+tar xvfz ../target/flume-ng-cassandra-sink-1.0.0-SNAPSHOT-dist.tar.gz > /dev/null
 pushd lib > /dev/null
 
-fileList="flume-cassandra-sink* \
+fileList="flume-ng-cassandra-sink*.jar \
       hector-core* \
-      cassandra-all* \
       guava* \
       speed4j* \
       uuid* \
-      snappy-java* \
-      compress-lzf* \
-      concurrentlinkedhashmap-lru* \
-      antlr* \
-      antlr-runtime* \
-      stringtemplate* \
-      jline* \
-      json-simple* \
-      snakeyaml* \
-      snaptree* \
-      metrics-core* \
       libthrift* \
       cassandra-thrift* \
-      je-5.0.58.jar \
-      hornetq-core-2.2.18.Final.jar \
 "
-
-
-
-
-
 
 cp -v ${fileList} ${FLUME_SERVER_LIB_DIR}/.
 cp -v ${fileList} ${FLUME_CLIENT_LIB_DIR}/.
-
-cp -v flume-cassandra-sink* ${FLUME_CLIENT_LIB_DIR}/.
-cp -v flume-cassandra-sink* /btoddb/flume-dist/avro-test-client/lib/.
 
 popd > /dev/null
 popd > /dev/null
